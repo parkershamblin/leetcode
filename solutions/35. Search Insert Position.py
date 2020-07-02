f93f842e7875b02eed70bd0a815f369ac34171ac
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-02 12:34:23 -0400

    64 ms

--- solutions/35. Search Insert Position.py
+++ solutions/35. Search Insert Position.py
@@ -1,9 +1,7 @@
+# REDO try to create solution with no try/except clauses
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        try:
-            return nums.index(target)
-        except ValueError as e:
-            for i in nums:
-                if i > target:
-                    return nums.index(i)
-            return len(nums)
+        for i in nums:
+            if i >= target:
+                return nums.index(i)
+        return len(nums)
