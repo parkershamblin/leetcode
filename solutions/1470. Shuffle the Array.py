5db4109654fe823874991a76f7c9003aacfd889d
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-02 10:42:24 -0400

    68 ms

--- solutions/1470. Shuffle the Array.py
+++ solutions/1470. Shuffle the Array.py
@@ -1,3 +1,16 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        start_1 = 0
+        start_2 = n
+        xs = [x for x in nums[start_1:start_2]]
+        ys = [y for y in nums[start_2:]]
+        result = []
+        for i in range(0, len(xs)):
+            result.append(xs[i])
+            result.append(ys[i])
+        return result
