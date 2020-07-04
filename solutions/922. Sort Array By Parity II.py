d1922b19e901799b5a51a6379772c6e715594770
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-04 04:08:19 -0400

    372 ms

---
+++ solutions/922. Sort Array By Parity II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortArrayByParityII(self, A: List[int]) -> List[int]:
+        res, evens, odds = [], [], []
+
+        for i in A:
+            if i % 2 == 0:
+                evens.append(i)
+            else:
+                odds.append(i)
+
+        for i in range(len(odds)):
+            res.extend([evens[i], odds[i]])
+
+        return res
