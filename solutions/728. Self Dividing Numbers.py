4146810cad622f84eb1afe77799be58122d009fc
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-07 06:31:29 -0400

    156 ms

--- solutions/728. Self Dividing Numbers.py
+++ solutions/728. Self Dividing Numbers.py
@@ -2,8 +2,6 @@ class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         res = []
         for i in range(left, right+1):
-            if "0" in str(i):
-                continue
-            if all([i % int(j) == 0 for j in str(i)]):
+            if all([int(j) != 0 and i % int(j) == 0 for j in str(i)]):
                 res.append(i)
         return res
