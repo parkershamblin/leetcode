2384581d7ce82143610f788e5b76f00a541a3926
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-06 22:45:28 -0400

    152 ms

--- solutions/942. DI String Match.py
+++ solutions/942. DI String Match.py
@@ -1,17 +1,17 @@
+# redo. Try to come up with a solution that does not requie a try/except clause
 class Solution:
     def diStringMatch(self, S: str) -> List[int]:
         res = []
-        cur = 0
-        end = len(S)
+        right, left = 0, len(S)
         for i in range(len(S)+1):
             try:
                 if S[i] == "I":
-                    res.append(cur)
-                    cur += 1
+                    res.append(right)
+                    right += 1
                 else:
-                    res.append(end)
-                    end -= 1
+                    res.append(left)
+                    left -= 1
             except IndexError as e:
-                res.append(cur)
-                cur += 1
+                res.append(right)
+                right += 1
         return res
