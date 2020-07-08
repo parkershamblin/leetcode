b02f45229359e197252aa6afba7294ab8c91c08c
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-08 09:31:57 -0400

    48 ms

--- solutions/804. Unique Morse Code Words.py
+++ solutions/804. Unique Morse Code Words.py
@@ -1,12 +1,14 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
+                 "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-",
+                 "-.--","--.."]
         alaphabet = string.ascii_lowercase
         mapping = {c: v for c, v in zip(alaphabet, morse)}
-        morse_words = []
+        morse_words = set()
         for word in words:
             morse_word = ""
             for c in word:
                 morse_word += mapping[c]
-            morse_words.append(morse_word)
-        return len(set(morse_words))
+            morse_words.add(morse_word)
+        return len(morse_words)
