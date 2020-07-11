2e6ea08c8ddc6a9f84e83c821bc9e9d369a90557
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-11 18:44:09 -0400

    36 ms

--- solutions/965. Univalued Binary Tree.py
+++ solutions/965. Univalued Binary Tree.py
@@ -7,18 +7,14 @@
 class Solution:
     def isUnivalTree(self, root: TreeNode) -> bool:
         stack = [root]
-        value = root.val
-        res = []
         while stack:
-            tmp =  stack.pop()
-            if tmp.val != value:
+            tmp = stack.pop()
+            if tmp.val != root.val:
                 return False
-            res.append(tmp.val)
             tmp_children = []
-            if tmp.left:
-                tmp_children.append(tmp.left)
-            if tmp.right:
-                tmp_children.append(tmp.right)
+            for child in [tmp.left, tmp.right]:
+                if child:
+                    tmp_children.append(child)
             for child in tmp_children[::-1]:
                 stack.append(child)
         return True
