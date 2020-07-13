7a19ee5a0f537d0533a1f74be61994e5e9008e18
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-13 12:17:38 -0400

    48 ms

--- solutions/1022. Sum of Root To Leaf Binary Numbers.py
+++ solutions/1022. Sum of Root To Leaf Binary Numbers.py
@@ -1,5 +1,3 @@
-# hint add an extra parameter and use the LSB to MSB trick (val = val * 2 + root.val) [Delete after first solved]
-# [do not look at notes unless you want spoiler]
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,8 +7,8 @@
 class Solution:
     def sumRootToLeaf(self, root: TreeNode, val=0) -> int:
         if not root:
-            return 0  # 0 or val
+            return 0
         val = val * 2 + root.val
         if not root.left and not root.right:
             return val
-        return self.sumRootToLeaf(root.left, val) + self.sumRootToLeaf(root.right, val)
+        return self.sumRootToLeaf(root.left, val)+self.sumRootToLeaf(root.right, val)
