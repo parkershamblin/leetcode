3f046f6c176f13c5c85f71b8b0628dd68455d34d
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-28 20:21:40 -0400

    52 ms

--- solutions/257. Binary Tree Paths.py
+++ solutions/257. Binary Tree Paths.py
@@ -5,11 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sumRootToLeaf(self, root: TreeNode) -> int:
-        def paths(root):
-            if not root:
-                return []
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        res = []
+​
+        def dfs(node, parent, path):
+            if not node:
+                return
+​
+            path = path + str(node.val) + "->"
+​
+            if not node.left and not node.right:
+                return path[:-2]
+​
+            left = dfs(node.left, node, path)
+            right = dfs(node.right, node, path)
+​
+            if left: res.append(left)
+            if right: res.append(right)
+        
+        # edge case handling for root empty tree and root node with no left or right child
+        if root:
             if not root.left and not root.right:
-                return [str(root.val)]
-            return [str(root.val) + str(i) for i in paths(root.left)] + [str(root.val) + i for i in paths(root.right)]
-        return sum(int(i, 2) for i in paths(root))
+                return [f"{root.val}"]
+​
+        # kick off recursive function on root
+        dfs(root, parent=None, path="")
+        return res
