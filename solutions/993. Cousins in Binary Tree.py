5093c8f4530ec8d132e1fb7438b56807223d34b2
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-29 21:48:11 -0400

    56 ms

--- solutions/993. Cousins in Binary Tree.py
+++ solutions/993. Cousins in Binary Tree.py
@@ -1,29 +1,26 @@
-# 
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+​
+from collections import namedtuple
+NodeAttributes = namedtuple('NodeAttributes', ['depth', 'parent'])
+​
 class Solution:
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
-        # return True if node1.depth = node2.depth and node1.parent != node2.parent
-        # traverse the tree level by level
-        current_level = [root]
-        while current_level:
-            next_level = []
-            for node in current_level:
-                if node.left: next_level.append((node.left, node.val))  # (node, parent)
-                if node.right: next_level.append((node.right, node.val))
-                if x in [node[0].val for node in next_level if node[0]]:
-                    if y in [node[0].val for node in next_level if node[0]]:
-                        for i, node in enumerate(next_level):
-                            if node[0].val == x:
-                                x_parent = node[1]
-                            elif node[0].val == y:
-                                y_parent = node[1]
-                        if x_parent != y_parent:
-                            return True
-            current_level = [node[0] for node in next_level]
-            next_level = []
-        return False
+        lookup = {}
+        stack = [(root, 0, None)]
+​
+        while stack:
+            node, depth, parent = stack.pop()
+            if node.val in (x, y):
+                lookup[node.val] = NodeAttributes(depth, parent)
+            if node.left:
+                stack.append((node.left, depth+1, node.val))
+            if node.right:
+                stack.append((node.right, depth+1, node.val))
+​
+        if lookup[x].depth == lookup[y].depth and lookup[x].parent != lookup[y].parent:
+            return True
