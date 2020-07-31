87709cc343f7e6a69d48314eeb911aa8310353ee
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-31 08:15:57 -0400

    32 ms

--- solutions/257. Binary Tree Paths.py
+++ solutions/257. Binary Tree Paths.py
@@ -7,27 +7,19 @@
 class Solution:
     def binaryTreePaths(self, root: TreeNode) -> List[str]:
         res = []
+    
+        def preorder(root, path):
+            if not root:
+                return ""
 ​
-        def dfs(node, parent, path):
-            if not node:
-                return
+            path += f"{str(root.val)}->"
 ​
-            path = path + str(node.val) + "->"
-​
-            if not node.left and not node.right:
-                return path[:-2]
-​
-            left = dfs(node.left, node, path)
-            right = dfs(node.right, node, path)
-​
-            if left: res.append(left)
-            if right: res.append(right)
-        
-        # edge case handling for root empty tree and root node with no left or right child
-        if root:
             if not root.left and not root.right:
-                return [f"{root.val}"]
-​
-        # kick off recursive function on root
-        dfs(root, parent=None, path="")
+                res.append(path[:-2])
+    
+            preorder(root.left, path)
+            preorder(root.right, path)
+        
+        # kick off recursive call on root
+        preorder(root, path="")
         return res
