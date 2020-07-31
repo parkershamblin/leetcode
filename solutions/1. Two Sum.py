3e36d02fd220c3c5f487c6cba210d5dae3df7850
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-07-31 05:48:19 -0400

    720 ms

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        stack = nums
-        while stack:
-            n = stack.pop()
-            if target - n in stack:
-                return [len(stack), stack.index(target-n)]
+        while nums:
+            n = nums.pop()
+            if target - n in nums:
+                return [len(nums), nums.index(target-n)]
