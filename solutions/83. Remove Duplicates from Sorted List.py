c178d3bbea089b8f1bd711f79097c58dcc40678e
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-02 00:03:08 -0400

    80 ms

--- solutions/83. Remove Duplicates from Sorted List.py
+++ solutions/83. Remove Duplicates from Sorted List.py
@@ -6,11 +6,12 @@
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
         cur = head
+        
         while cur:
-            # Uses next_distinct to find the next distinct value.
             next_distinct = cur.next
             while next_distinct and next_distinct.val == cur.val:
                 next_distinct = next_distinct.next
             cur.next = next_distinct
             cur = next_distinct
+        
         return head
