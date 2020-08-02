39321fa5b85e2fc12142130ce94fe662005b9dd8
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-02 01:23:20 -0400

    28 ms

--- solutions/993. Cousins in Binary Tree.py
+++ solutions/993. Cousins in Binary Tree.py
@@ -4,23 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-​
-from collections import namedtuple
-NodeAttributes = namedtuple('NodeAttributes', ['depth', 'parent'])
-​
 class Solution:
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
         lookup = {}
         stack = [(root, 0, None)]
-​
+        
         while stack:
             node, depth, parent = stack.pop()
-            if node.val in (x, y):
-                lookup[node.val] = NodeAttributes(depth, parent)
-            if node.left:
-                stack.append((node.left, depth+1, node.val))
+            if node and node.val in (x, y):
+                lookup[node.val] = (depth, parent)
             if node.right:
-                stack.append((node.right, depth+1, node.val))
-​
-        if lookup[x].depth == lookup[y].depth and lookup[x].parent != lookup[y].parent:
-            return True
+                stack.append((node.right, depth+1, node))
+            if node.left:
+                stack.append((node.left, depth+1, node))
+        return lookup[x][0] == lookup[y][0] and lookup[x][1] != lookup[y][1]
