92271cad887086b14fd07b8e24103c8c4caef346
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-02 02:30:25 -0400

    48 ms

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -1,6 +1,8 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        while nums:
-            n = nums.pop()
-            if target - n in nums:
-                return [len(nums), nums.index(target-n)]
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+        for i, n in enumerate(nums):
+            if (target - n) in d:
+                return [d[target-n], i]
+            else:
+                d[n] = i
