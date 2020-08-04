bddccce3fcd2a80469b61c5528afaaa1c8fc63d4
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-04 01:50:04 -0400

    96 ms

--- solutions/136. Single Number.py
+++ solutions/136. Single Number.py
@@ -1,6 +1,6 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        
-        for n in nums:
-            if nums.count(n) != 2:
-                return n
+    def singleNumber(self, nums: List[int]) -> int:
+        c = collections.Counter(nums)
+        for k, v in c.items():
+            if v == 1:
+                return k
