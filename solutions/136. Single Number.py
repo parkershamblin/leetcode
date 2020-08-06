08dff66d978d0ae4da8f35c07c16309efe5adc97
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-06 04:41:29 -0400

    80 ms

--- solutions/136. Single Number.py
+++ solutions/136. Single Number.py
@@ -1,5 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        
+        # Approach 1: bit manipulation
+        a = 0
+        for n in nums:
+            a ^= n
+        return a
+​
+​
+        # Approach 2:  Math
+        return 2 * sum(set(nums)) - sum(nums)
+​
+​
+        # Approach 3: hash table
         c = collections.Counter(nums)
         for k, v in c.items():
             if v == 1:
