edac3d2d97efdca6eb0ae7c766a0814d9e4bf613
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-06 04:44:23 -0400

    60 ms

--- solutions/1512. Number of Good Pairs.py
+++ solutions/1512. Number of Good Pairs.py
@@ -1,17 +1,4 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        # n*(n+1)/2
-        # the sum of a list of positive consectuive numbers up to n is equal to average of the number elem??** forget
-        """cant do this way. forget logic
-        import collections
-        collections.Counter(nums)
-        """
-        d = {}
-        res = 0
-        for n in nums:
-            if n in d:
-                res += d[n]
-                d[n] += 1
-            else:
-                d[n] = 1
-        return res
+        return sum((n*(n-1)//2) for n in collections.Counter(nums).values())
+​
