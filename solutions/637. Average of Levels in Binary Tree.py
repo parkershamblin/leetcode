1e0ab6b96daa2b54e1d7bc5e6e60f14052621b22
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-06 10:30:27 -0400

    56 ms

--- solutions/637. Average of Levels in Binary Tree.py
+++ solutions/637. Average of Levels in Binary Tree.py
@@ -1,21 +1,25 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def averageOfLevels(self, root: TreeNode) -> List[float]:
-        result = []
-        current_level = [root]
-        
-        while len(current_level) > 0:
-            result.append(sum(node.val for node in current_level)/len(current_level))
-            next_level = []
-            for node in current_level:
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-            current_level = next_level
-        return result
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root:
+            return 0
+        cur_level = [root]
+        level_values = []
+        while cur_level:
+            next_level = []
+            cur_level_values = []
+            for node in cur_level:
+                if node:
+                    cur_level_values.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            level_values.append(cur_level_values)
+            cur_level = next_level
+        return [sum(level)/len(level) for level in level_values]
