25e9a9f76fe6a5ed6eb0243392341de2b19acd06
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-06 07:47:07 -0400

    40 ms

--- solutions/20. Valid Parentheses.py
+++ solutions/20. Valid Parentheses.py
@@ -1,18 +1,12 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        # this was my own solution
-        stack = []
-        mapping = {')': '(', ']': '[', '}': '{'}
-        
-        for i in s:
-            if i in mapping.values():
-                stack.append(i)
-            elif stack:
-                if stack[-1] == mapping[i]:
-                    stack.pop()
-                else:
-                    return False
-            else:
-                return False
-        if len(stack) == 0:
-            return True
+    def isValid(self, s: str) -> bool:
+        stack = []
+        lookup = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in lookup.values():
+                stack.append(char)
+            if char in lookup.keys():
+                if not stack or stack.pop() != lookup[char]:
+                    return False
+        if len(stack) == 0:
+            return True
