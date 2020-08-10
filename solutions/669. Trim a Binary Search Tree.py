50c11c6c5ad36679a7761472e2ed131c443a6d8f
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-10 13:11:10 -0400

    40 ms

--- solutions/669. Trim a Binary Search Tree.py
+++ solutions/669. Trim a Binary Search Tree.py
@@ -7,12 +7,12 @@
 class Solution:
     def trimBST(self, root: TreeNode, L: int, R: int) -> TreeNode:
         if not root:
-            return
-        elif root.val < L:
+            return None
+        if root.val < L:
             return self.trimBST(root.right, L, R)
-        elif root.val > R:
+        if root.val > R:
             return self.trimBST(root.left, L, R)
-        else:
-            root.left = self.trimBST(root.left, L, R)
-            root.right = self.trimBST(root.right, L, R)
-            return root
+​
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        return root
