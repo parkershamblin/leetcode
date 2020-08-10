9b56f8e764411ec76e357f2555785d6faf0d0f8a
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-10 15:13:08 -0400

    36 ms

--- solutions/107. Binary Tree Level Order Traversal II.py
+++ solutions/107. Binary Tree Level Order Traversal II.py
@@ -8,15 +8,18 @@ class Solution:
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return
-        current_level = [root]
-        levels = []
-        while current_level:
+​
+        res = []
+        cur_level = [root]
+        
+        while cur_level:
             next_level = []
-            levels.append([node.val for node in current_level if node])
-            for node in current_level:
+            res.append([node.val for node in cur_level])
+            for node in cur_level:
                 if node.left:
                     next_level.append(node.left)
                 if node.right:
                     next_level.append(node.right)
-            current_level = next_level
-        return reversed([level for level in levels])
+            cur_level = next_level
+​
+        return res[::-1]
