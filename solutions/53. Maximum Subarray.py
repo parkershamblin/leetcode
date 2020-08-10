9f6514812ec99979c9bf702b85069029669e5268
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-10 12:15:45 -0400

    64 ms

--- solutions/53. Maximum Subarray.py
+++ solutions/53. Maximum Subarray.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        cur_max = tot_max = nums[0]
+        cur_max = total_max = nums[0]
         for n in nums[1:]:
-            cur_max = max(cur_max+n, n)
-            tot_max = max(cur_max, tot_max)
-        return tot_max
+            cur_max = max(cur_max + n, n)
+            total_max = max(total_max, cur_max)
+        return total_max
