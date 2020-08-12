f8acac8bde3be3d31787b250f0f4c7040c63ae40
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-12 13:16:57 -0400

    44 ms

--- solutions/13. Roman to Integer.py
+++ solutions/13. Roman to Integer.py
@@ -1,33 +1,22 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        res = 0
-        if "IV" in s:
-            res += 4
-            s = s.replace("IV", "")
-        if "IX" in s:
-            res += 9
-            s = s.replace("IX", "")
-        if "XL" in s:
-            res += 40
-            s = s.replace("XL", "")
-        if "XC" in s:
-            res += 90
-            s = s.replace("XC", "")
-        if "CD" in s:
-            res += 400
-            s = s.replace("CD", "")
-        if "CM" in s:
-            res += 900
-            s = s.replace("CM", "")
-        res += s.count("I")
-        res += s.count("V") * 5
-        res += s.count("X") * 10
-        res += s.count("L") * 50
-        res += s.count("C") * 100
-        res += s.count("D") * 500
-        res += s.count("M") * 1000
-        return res
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        hash_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        
+        for i, c in enumerate(s):
+            if i > 0:
+                if c == "V" and s[i-1] == "I":
+                    res -= 2 * hash_table[s[i-1]]
+                if c == "X" and s[i-1] == "I":
+                    res -= 2 * hash_table[s[i-1]]
+                if c == "L" and s[i-1] == "X":
+                    res -= 2 * hash_table[s[i-1]]
+                if c == "C" and s[i-1] == "X":
+                    res -= 2 * hash_table[s[i-1]]
+                if c == "D" and s[i-1] == "C":
+                    res -= 2 * hash_table[s[i-1]]
+                if c == "M" and s[i-1] == "C":
+                    res -= 2 * hash_table[s[i-1]]
+            res += hash_table[c]
+        
+        return res
