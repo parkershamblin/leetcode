aaf68a71217c920bdb2630a146f94df4450d2967
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-13 15:36:58 -0400

    28 ms

--- solutions/1046. Last Stone Weight.py
+++ solutions/1046. Last Stone Weight.py
@@ -2,6 +2,6 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         h = [-stone for stone in stones]
         heapq.heapify(h)
-        while len(h) > 1 and h[0] != 0:
+        while h[0] != 0 and len(h) > 1:
             heapq.heappush(h, heapq.heappop(h) - heapq.heappop(h))
         return -h[0]
