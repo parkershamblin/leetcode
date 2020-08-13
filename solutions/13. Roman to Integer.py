7cdd4127600f416fb09fe7159ba210c3db9259a7
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-13 18:05:40 -0400

    44 ms

--- solutions/13. Roman to Integer.py
+++ solutions/13. Roman to Integer.py
@@ -1,22 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        res = 0
-        hash_table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        
-        for i, c in enumerate(s):
-            if i > 0:
-                if c == "V" and s[i-1] == "I":
-                    res -= 2 * hash_table[s[i-1]]
-                if c == "X" and s[i-1] == "I":
-                    res -= 2 * hash_table[s[i-1]]
-                if c == "L" and s[i-1] == "X":
-                    res -= 2 * hash_table[s[i-1]]
-                if c == "C" and s[i-1] == "X":
-                    res -= 2 * hash_table[s[i-1]]
-                if c == "D" and s[i-1] == "C":
-                    res -= 2 * hash_table[s[i-1]]
-                if c == "M" and s[i-1] == "C":
-                    res -= 2 * hash_table[s[i-1]]
-            res += hash_table[c]
-        
-        return res
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        result = 0
+        for i in range(len(s) - 1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                result -= roman[s[i]]
+            else:
+                result += roman[s[i]]
+        return result + roman[s[-1]]
