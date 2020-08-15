1cc91220ba9c4dada6b97d262106f8b2a5495c88
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-15 19:27:58 -0400

    32 ms

--- solutions/125. Valid Palindrome.py
+++ solutions/125. Valid Palindrome.py
@@ -1,4 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join([c for c in s if c.isalpha() or c.isdigit()]).lower()
+        s = s.lower()
+        s = ''.join([c for c in s if c.isalnum()])
         return s[::-1] == s
