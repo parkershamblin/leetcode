775ce0f277b861edaa7a29b60a51bdd0e83a3a17
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-15 10:51:34 -0400

    120 ms

--- solutions/1528. Shuffle String.py
+++ solutions/1528. Shuffle String.py
@@ -1,6 +1,6 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         res = [''] * len(s)
-        for i in range(len(s)):
+        for i in range(len(res)):
             res[indices[i]] = s[i]
         return ''.join(c for c in res)
