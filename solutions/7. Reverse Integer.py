74a005ab1f7def563705cf7677ad211bf4787efd
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-15 18:43:19 -0400

    28 ms

---
+++ solutions/7. Reverse Integer.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = (x > 0) - (x < 0)
+        number = int(str(x * sign)[::-1])
+        return sign * number if number < pow(2, 31) else 0
+​
