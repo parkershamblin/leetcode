887aebd157552917dfc8901a81e1f3f1e6449f2c
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-21 17:56:42 -0400

    112 ms

--- solutions/167. Two Sum II - Input array is sorted.py
+++ solutions/167. Two Sum II - Input array is sorted.py
@@ -2,7 +2,8 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         lookup = {}
         for i, n in enumerate(numbers):
-            if (target - n) in lookup:
-                return [lookup[target - n] + 1, i + 1]  # not zero-based index
+            if target - n in lookup:
+                return [lookup[target - n] + 1, i + 1]
             else:
                 lookup[n] = i
+​
