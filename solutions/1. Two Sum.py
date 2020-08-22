83b7cce85170741e72ef1d050b6fa23ff609e46b
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-22 06:11:32 -0400

    52 ms

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums, target):
         lookup = {}
         for i, n in enumerate(nums):
-            if (target - n) in lookup:
-                return [lookup[(target - n)], i]
+            if target - n in lookup:
+                return [i, lookup[target - n]]
             else:
                 lookup[n] = i
 ​
