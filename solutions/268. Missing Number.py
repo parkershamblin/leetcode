819d9efa7aec7bce86a5c594112fff220095efb0
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-21 21:27:25 -0400

    176 ms

--- solutions/268. Missing Number.py
+++ solutions/268. Missing Number.py
@@ -1,5 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # gauss' formula math approach
         n = len(nums)
-        tmp = n * (n + 1) // 2
-        return tmp - sum(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
+​
+    def missingNumber1(self, nums: List[int]) -> int:
+        # bit manipulation approach
+        a = 0
+        for i in range(len(nums) + 1):
+            a ^= i
+        for n in nums:
+            a ^= n
+        return a
+​
