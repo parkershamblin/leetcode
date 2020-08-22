f195bd138758083acefac60fe6d9decc2c96f60e
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-21 21:42:05 -0400

    52 ms

--- solutions/102. Binary Tree Level Order Traversal.py
+++ solutions/102. Binary Tree Level Order Traversal.py
@@ -6,8 +6,8 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        res, level = [], [root]
-        while root and level:
-            res.append([node.val for node in level])
-            level = [child for node in level for child in (node.left, node.right) if child]
-        return res
+        result = []
+11
+12
+13
+14
