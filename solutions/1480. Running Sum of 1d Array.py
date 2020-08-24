063087f66fa19f0a1d77c821f721d9b63046213c
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-24 15:07:55 -0400

    96 ms

--- solutions/1480. Running Sum of 1d Array.py
+++ solutions/1480. Running Sum of 1d Array.py
@@ -1,4 +1,7 @@
+import numpy as np
+​
+​
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        running_sum = [sum(nums[:i+1]) for i in range(len(nums))]
-        return running_sum
+    def runningSum(self, nums: List[int]) -> List[int]:
+        return np.cumsum(nums)
+​
