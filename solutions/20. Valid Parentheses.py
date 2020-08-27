4ba7d1c33c250ceead1db353fc6d0309108d95d3
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2020-08-26 22:56:53 -0400

    28 ms

--- solutions/20. Valid Parentheses.py
+++ solutions/20. Valid Parentheses.py
@@ -2,12 +2,10 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         lookup = {")":"(", "}":"{", "]":"["}
-        
         for char in s:
-            if char in lookup.values():
-                stack.append(char)
-            else:
+            if char in lookup.keys():
                 if not stack or stack.pop() != lookup[char]:
                     return False
-​
+            else:
+                stack.append(char)
         return len(stack) == 0
