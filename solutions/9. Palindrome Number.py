015a1e5a3a07d88778caab6ffa4c6b440d0d875d
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2022-04-17 00:12:12 -0400

    66 ms

--- solutions/9. Palindrome Number.py
+++ solutions/9. Palindrome Number.py
@@ -1,9 +1,3 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        # solution with converting int to string
-        if x >= 0:
-            res = int(str(x)[::-1])
-            if res == x:
-                return True
-        else:
-            return False
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
