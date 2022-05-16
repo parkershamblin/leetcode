5a941bb58a37ea457b047783fcd64b5cb1ccfc46
Parker Shamblin <53675680+parkershamblin@users.noreply.github.com>
2022-05-16 16:00:54 -0400

    371 ms

--- solutions/704. Binary Search.py
+++ solutions/704. Binary Search.py
@@ -1,25 +1,12 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        
-        if 1 <= len(nums) <= 2:
-            if nums[0] == target:
-                return 0
-            elif nums[-1] == target:
-                return 1
-            
-        left, mid, right = 0, len(nums) // 2, len(nums)-1
-        while left < mid < right:
-            print(right)
-            if nums[mid] == target:
-                return mid
-            elif nums[left] == target:
-                return left
-            elif nums[right] == target:
-                return right
-            elif nums[mid] > target:
-                right = mid
-                mid = (left + right) // 2
-            elif nums[mid] < target:
-                left = mid
-                mid = (left + right) // 2
-        return -1
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = (0), (len(nums) - 1)
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+        return -1
